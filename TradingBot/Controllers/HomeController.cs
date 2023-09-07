7df00aa978c209b345fd58db0eef7d58b7ed965f
An Khanh Tran <ktran2@iwu.edu>
2023-09-07 00:56:14 -0500

    updated codes for HomePage

--- TradingBot/Controllers/HomeController.cs
+++ TradingBot/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace TradingBot.Controllers
         {
             return View();
         }
+        
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
     }   
 }
